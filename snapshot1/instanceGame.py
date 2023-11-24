cde0bcbc1718cfcea4a7077c9ed929a8a41eaaeb
Gaton128397 <gvillanueva19@alumnos.utalca.cl>
2023-11-23 22:43:42 -0300

    Se arreglo:
    reinicio
    aparicion tanquesposibilidad para mas jugadores

--- snapshot1/instanceGame.py
+++ snapshot1/instanceGame.py
@@ -1,21 +1,52 @@
 from random import randint
-import pygame,projectile,tank,nTerrain,time,sys,chooseMenu, params, drawFunctions,  infoBlock
+import pygame,projectile,nTank,nTerrain,time,sys,chooseMenu, params, drawFunctions,  infoBlock
 
 class gameLogic:
     
     def __init__(self, windowGame):
         self.screen = windowGame
+        
         #background
         self.backGround = pygame.Surface((params.WIDTH,params.HEIGHT))
         drawFunctions.backgroundDraw(self.backGround)
         self.screen.blit(self.backGround,(0,0))
+        
         #Terreno
         self.terrain = nTerrain.TerrenoVariado(params.WIDTH, params.HEIGHT)
         self.surfaceTerrain = self.terrain.drawTerrain()
         self.screen.blit(self.surfaceTerrain,(0,0))
         
+        #players
+        self.surfaceJugadores = pygame.Surface((params.WIDTH,params.HEIGHT))
+        self.surfaceJugadores.fill((255,0,255))
+        self.surfaceJugadores.set_alpha()
+        self.surfaceJugadores.set_colorkey((255,0,255))
+        self.listaJugadores = []
+        self.setPlayers()
+        self.updPlayers()
+            
+    def setPlayers(self):
+        splitPos = params.WIDTH//params.playersNumber
+        contador = 0
+        for i in range(params.playersNumber-1):
+            self.listaJugadores.append(nTank.Tank((randint(contador,contador+splitPos),100),"blue",randint(1,2),self.surfaceJugadores,self.terrain.getPoints()))
+            contador += splitPos
+            
+    def updPlayers(self):
+        for i in range(len(self.listaJugadores)):
+            self.listaJugadores[i].draw_tank(True)
+        self.screen.blit(self.surfaceJugadores,(0,0))
         
+    def unUpdate(self):
+        self.screen.blit(self.backGround,(0,0))
+        self.screen.blit(self.surfaceTerrain,(0,0))
         
+    def pauseScreen(self):
+        print("pause state")
+    
+    def loadingScreen():
+        print("loading")
+    
     def run(self,clock):
         playerWinner = -1
         running = True
@@ -28,8 +59,12 @@ class gameLogic:
                 if pygame.mouse.get_pressed()[0]:
                     pygame.draw.circle(self.surfaceTerrain, (255, 0, 255), pygame.mouse.get_pos(), 100)
                     print("hit")
-                    self.screen.blit(self.backGround,(0,0))
-                    self.screen.blit(self.surfaceTerrain,(0,0))
+                    self.unUpdate()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        playerWinner = 1
+                        return playerWinner
+                    
             #clock.tick(60)
             
             pygame.display.update()
@@ -38,11 +73,11 @@ class gameLogic:
 def tstgm():
     clock = pygame.time.Clock()
     window = pygame.display.set_mode((params.WIDTH, params.HEIGHT))
-    game = gameLogic(window)
     playerWon = None
     run = True
     while run:
         for event in pygame.event.get():
+            game = gameLogic(window)
             playerWon = game.run(clock)
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -50,6 +85,9 @@ def tstgm():
             elif playerWon == -1:
                 pygame.quit()
                 run = False
+            if playerWon == 1:
+                print("l")
+                
         print("not reach")
         clock.tick(60)
         pygame.display.update()
---
+++ snapshot1/nTank.py
@@ -0,0 +1,126 @@
+import pygame, sys, math, random
+from functions import *
+
+class Tank:
+    def __init__(self,position, color,LoR,surface,terrainPoints):
+
+        self.health = 100
+
+        self.x = position[0] - 15
+        self.y = position[1] - 25
+        self.width = 50
+        self.height = 20
+        self.color = color
+        self.terrainPoints = terrainPoints
+        self.origin = (position[0] + 25, position[1] - 6.5)
+
+        self.angulo = 0
+        self.longitud = 25 #largo del cañon
+
+        self.surface = surface
+        self.LoR = LoR
+
+        self.xCanon1 = 0
+        self.yCanon1 = 0
+
+        self.xCanon2 = 0
+        self.yCanon2 = 0
+
+        self.end = (0,0)
+        a =1
+        if self.LoR == 0:
+            a = -1
+
+        self.xCanon1 =(self.x + self.width/2)  #coordenadas del cañon
+        self.yCanon1  = (self.y - self.height/3)
+
+        self.xCanon2 = (self.xCanon1 + a*self.longitud * math.cos(math.radians(self.angulo)))
+        self.yCanon2 = (self.yCanon1 - self.longitud * math.sin(math.radians(self.angulo)))
+        
+        self.ammo10mm = 3
+        self.ammo8mm = 10
+        self.ammo6mm = 3
+        while self.y < self.terrainPoints[int(self.x+self.width*0.5)][1]-20:
+            self.y += 0.1
+            self.yCanon1 += 0.1
+            self.yCanon2 += 0.1
+    def draw_tank(self,staticCan):
+        pygame.draw.rect(self.surface, self.color, (self.x, self.y, self.width, self.height)) #rectangulo inicial
+        pygame.draw.rect(self.surface, self.color, (self.x + self.width/4, self.y - self.height/5, self.width/2, self.height/2)) #circunferencia de la izquierda
+        pygame.draw.circle(self.surface, self.color, ((self.x, self.y + self.height/2)), self.height/2) #circunferencia de la derecha
+        pygame.draw.circle(self.surface, self.color, ((self.x + self.width, self.y + self.height/2)), self.height/2) #rectangulo donde estara el cañon
+        while self.y < self.terrainPoints[int(self.x+self.width*0.5)][1]-20:
+            self.y += 0.1
+        if(staticCan):
+            pygame.draw.line(self.surface, self.color, (self.xCanon1, self.yCanon1), (self.xCanon2, self.yCanon2), 4) #cañon
+    def actualizar(self,a):
+            
+            if self.LoR == 0:
+                a = -1
+
+            self.xCanon2 = self.xCanon1 + a*self.longitud * math.cos(math.radians(self.angulo))
+            self.yCanon2 = self.yCanon1 - self.longitud * math.sin(math.radians(self.angulo))
+            self.end = (self.xCanon2,self.yCanon2)
+
+    #funcion para mover el canon del tanque    
+    def moveCannon(self,temp):
+        self.actualizar(self.LoR)
+        
+        if 90 > self.angulo:
+            keys1 = pygame.key.get_pressed()
+            if keys1[pygame.K_UP]:
+
+                self.angulo += 1
+                self.surface.blit(temp[0],(0,0))
+                pygame.draw.line(self.surface,self.color,(self.xCanon1, self.yCanon1), (self.xCanon2, self.yCanon2), 4)
+                
+        if 0 < self.angulo:
+            keys1 = pygame.key.get_pressed()        
+            if keys1[pygame.K_DOWN]:
+                    #("algo abajo")
+                    self.angulo -= 1
+                    self.surface.blit(temp[0],(0,0))
+                    pygame.draw.line(self.surface,self.color,(self.xCanon1, self.yCanon1), (self.xCanon2, self.yCanon2), 4)
+
+    # funcion con hitbox del tanque              
+    def hitBox(self):
+        hitboxPoints = []
+        hitboxInitialX=self.x-10
+        hitboxInitialY=self.y-4
+        hitboxWidth = 70
+        hitboxHeight = 30
+
+        for i in range(hitboxWidth):
+            for j in range(hitboxHeight):
+                hitboxPoints.append((hitboxInitialX + i, hitboxInitialY + j))
+
+        ##(hitboxPoints[0])
+        return hitboxPoints
+    # def chooseAmmo(self):
+    #     for event in pygame.event.get():
+    #          if event.type == pygame.KEYDOWN:
+    #             if event.key == pygame.K_1:
+    #                 #(1)
+    #             if event.key == pygame.K_2:
+    #                 #(2)
+    #             if event.key == pygame.K_3:
+    #                 #(3)
+              
+    def getAngle(self):
+        
+        return self.angulo
+    
+    #funcion para obtener la vida del tanque
+    def getHeath(self):
+        return self.health
+    
+    #funcion para restar vida al tanque
+    def loseHealth(self, type):
+        if type == 1:
+            self.health -= 50
+        elif type == 2:
+            self.health -= 40
+        elif type == 3:
+            self.health -= 30
+        return self.health
+         
