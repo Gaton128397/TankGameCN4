e1baa7e20108f0b3eff5b888420a1913947f7d36
Marusz <mamunoz22@alumnos.utalca.cl>
2023-11-27 19:44:49 -0300

    tienda, items, pequenos arreglos y mejoras

--- snapshot1/params.py
+++ snapshot1/params.py
@@ -1,5 +1,10 @@
 import pygame
 WIDTH = 1280
 HEIGHT = 720
-BackGroundIMG = "snapshot1/Background.jpg"
+BackGroundIMG = "snapshot1/imgs/Background.jpg"
+shopImg = "snapshot1/imgs/shop.png"
+playmodeImg = "snapshot1/imgs/playmode.jpg"
+settingsImg = "snapshot1/imgs/settings.jpg"
+homeIco = "snapshot1/imgs/home.png"
+settingsIco = "snapshot1/imgs/settingsICO.png"
 playersNumber = 6
