6d4566353b07b7b08ccdf050a1663bbc64ba36ec
pinxulai <javier.sanchezd@gmail.com>
2023-12-01 00:39:17 -0300

    fase final de pantalla, clase y seteo de botones sin numeros magicos. FALTA UNIR A LA LOGICA DE PANTALLAS

--- runMaps.py
+++ runMaps.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-import button
+import button,params
 
 def run_maps():
     pygame.init()
@@ -9,22 +9,24 @@ def run_maps():
     width, height = 16*x, 9*x
     screen = pygame.display.set_mode((width, height))
 
-    maps_background = pygame.image.load('imgs/mapas.png')  
-    maps_background = pygame.transform.scale(maps_background, (width, height))
+    maps_background = params.bgMapas
     screen.blit(maps_background, (0, 0))
 
     clock = pygame.time.Clock()
 
-    home_button = button.Button((x*0.5 - 10, x*0.5 - 20, 100, 100), (255, 0, 0), 'Home', False)
-    settings_button = button.Button((width - x*1.5 + 15, x*0.5 - 25, 100, 100), (255, 0, 0), 'Settings', False)
-    confirmar_button = button.Button((width // 2 - 150, height // 2 - 50, 300, 100), (0, 255, 0), 'Confirmar', False)
+    home_button = button.Button((x*0.5 - 10, x*0.5 - params.size*0.2, 100, 100), (255, 0, 0), 'Home', False)
+    settings_button = button.Button((width - x*1.5 + params.size*0.15, x*0.5 - params.size*0.25, 100, 100), (255, 0, 0), 'Settings', False)
+    confirmar_button = button.Button((width - params.size*3, height - params.size*1, 250, 80), (0, 255, 0), 'Confirmar', False)
 
-    random_map_button = button.Button((width // 2 - 100, height // 2 - 200, 200, 200), (0, 255, 0), 'Random Map', False)
-    selva_button = button.Button((width // 2 - 300, height // 2 - 100, 200, 200), (0, 255, 0), 'Selva', False)
-    galaxy_button = button.Button((width // 2 - 100, height // 2 - 100, 200, 200), (0, 255, 0), 'Galaxy', False)
-    nieve_button = button.Button((width // 2 + 100, height // 2 - 100, 200, 200), (0, 255, 0), 'Nieve', False)
-    desierto_button = button.Button((width // 2 - 300, height // 2 + 100, 200, 200), (0, 255, 0), 'Desierto', False)
-    ciudad_button = button.Button((width // 2 - 100, height // 2 + 100, 200, 200), (0, 255, 0), 'Ciudad', False)
+
+
+    random_map_button = button.Button((width // 2 - params.size*6.15, height // 2 - params.size*1.85, 300, 175), (0, 255, 0), 'Random Map', False)
+    selva_button = button.Button((width // 2 - params.size*2.35, height // 2 - params.size*1.85, 300, 175), (0, 255, 0), 'Selva', False)
+    galaxy_button = button.Button((width // 2 + params.size*1.55, height // 2 - params.size*1.85, 300, 175), (0, 255, 0), 'Galaxy', False)
+
+    nieve_button = button.Button((width // 2 - params.size*6.15, height // 2 + params.size*1.25, 300, 175), (0, 255, 0), 'Nieve', False)
+    desierto_button = button.Button((width // 2 - params.size*2.4, height // 2 + params.size*1.25, 300, 175), (0, 255, 0), 'Desierto', False)
+    ciudad_button = button.Button((width // 2 + params.size*1.6, height // 2 + params.size*1.25, 300, 175), (0, 255, 0), 'Ciudad', False)
 
     buttons = [home_button, settings_button, confirmar_button,
                random_map_button, selva_button, galaxy_button,
