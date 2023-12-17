4e4777f282089a7828c2d1197e970989c8cf83c4
pinxulai <javier.sanchezd@gmail.com>
2023-12-16 21:17:15 -0300

    commit para hacer merge

--- params.py
+++ params.py
@@ -3,10 +3,7 @@ playersNumber = 2
 roundNumber = 1
 
 gravityConstant = 9.8
-
 size = 100
-playersNumber = 2
-roundNumber = 2
 screen = display.set_mode((size*16, size*9))
 #icons
 shield = image.load('items/shield.png')
