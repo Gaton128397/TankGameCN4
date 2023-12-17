0fb9ab636672a2d1f64c57971ab5fc9625fcfe3e
pinxulai <javier.sanchezd@gmail.com>
2023-12-17 14:37:20 -0300

    Comentarios codigo

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from random import randint
 class Player:
     def __init__(self):
+        # Inicialización de atributos para un jugador
         self.money = 0
         self.health = 0
         self.dmg = 0
@@ -14,15 +15,17 @@ class Player:
         self.deaths = 0
         self.tanque = None
         self.selfKill = False
-        self.ia = False
+        self.ia = False  # ¿Es un jugador controlado por la IA?
         
         self.kda = [0,0]#indice 0 kills, indice 1 deaths
         self.generalkda = [0,0]#indice 0 points, indice 1 kills, indice 2 deaths
         
     def asignTank(self,tank):
+        # Asigna un tanque al jugador
         self.tanque = tank
         
     def returnKDA(self):
+        # Devuelve las estadísticas KDA del jugador
         return self.kda
 
                 
